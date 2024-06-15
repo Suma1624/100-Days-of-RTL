d54af5c3e3fc22902ee73c1a597d4f1ac46f4fd6
Suma <162892414+Suma1624@users.noreply.github.com>
2024-06-15 20:15:25 +0530

    Add files via upload

---
+++ day43_1001 mealy sequence detetector/overlapping mealy sequence detector/fsm1001_ov2.v
@@ -0,0 +1,41 @@
+`timescale 1ns / 1ps
+
+module fsm1001_ov2(clk,rst,in,out);
+input clk,rst,in;
+output out;
+parameter S0 = 4'b0000,
+S1 = 4'b0001,
+S2 = 4'b0010,
+S3 = 4'b0011;
+reg [3:0]state,nextstate;
+
+always@(posedge clk) begin
+if (rst) state <= S0;
+else state <= nextstate;
+end
+
+always@(state,in) begin
+case(state)
+S0: begin 
+if (in) nextstate <= S1; 
+else nextstate <= S0;
+end
+S1: begin
+if (in) nextstate <= S1;
+else nextstate <= S2;
+end
+S2: begin
+if (in) nextstate <= S1;
+else nextstate <= S3;
+end
+S3: begin
+if (in) nextstate <= S1;
+else nextstate <= S0;
+end
+default: nextstate <= S0;
+endcase
+end
+
+assign out = (state == S3) && (in == 0)?1:0;
+
+endmodule
---
+++ day43_1001 mealy sequence detetector/overlapping mealy sequence detector/fsm1001_ov2_tb.v
@@ -0,0 +1,33 @@
+`timescale 1ns / 1ps
+
+module fsm1001_ov2_tb;
+reg clk,rst,in;
+wire out;
+fsm1001_ov2 DUT(clk,rst,in,out);
+
+initial begin
+clk = 1'b0;
+forever #5 clk = ~clk;
+end
+
+initial begin
+rst = 1'b1;
+in = 1'b0;
+#10 rst = 1'b0;
+#10 in = 1'b1;
+#10 in = 1'b0;
+#10 in = 1'b0;
+#10 in = 1'b1;
+#10 in = 1'b0;
+#10 in = 1'b1;
+#10 in = 1'b0;
+#10 in = 1'b0;
+#10 in = 1'b1;
+#10 in = 1'b0;
+end
+
+initial begin
+$monitor("clk: %b input: %b output: %b", clk,in,out);
+#140 $finish;
+end
+endmodule
